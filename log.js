d2bd6aaaa016d191313dcec10f5034fbc3ea6128
xuyang <xuyang@wozlla.com>
2015-06-29 20:52:31 +0800

    日志格式，备份

--- log.js
+++ log.js
@@ -8,22 +8,25 @@ var log = bunyan.createLogger({
     },
     {
       level: 'info',
+      type: 'rotating-file',
       path: './logs/info.log', // log ERROR and above to a file
-      period: '1m',   // daily rotation
+      period: '1d',   // daily rotation
       count: 7  ,     // keep 3 back copies
       template: './logs/info.$time.log'
     },
     {
       level: 'warn',
+      type: 'rotating-file',
       path: './logs/warn.log', // log ERROR and above to a file
-      period: '1m',   // daily rotation
+      period: '1d',   // daily rotation
       count: 7  ,     // keep 3 back copies
       template: './logs/warn.$time.log'
     },
     {
       level: 'error',
+      type: 'rotating-file',
       path: './logs/error.log', // log ERROR and above to a file
-      period: '1m',   // daily rotation
+      period: '1d',   // daily rotation
       count: 7  ,     // keep 3 back copies
       template: './logs/error.$time.log'
     }
